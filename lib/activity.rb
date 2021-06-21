8d1db5bbc6e6083c07eea1002a1ccd1fe06b6f83
Tanner Dale <tanner@tannerdale.com>
2021-06-21 13:06:57 -0500

    Add add participant and total cost methods

--- lib/activity.rb
+++ lib/activity.rb
@@ -5,4 +5,12 @@ class Activity
     @name = name
     @participants = {}
   end
+
+  def add_participant(name, cost)
+    @participants[name] = cost
+  end
+
+  def total_cost
+    @participants.values.sum
+  end
 end
