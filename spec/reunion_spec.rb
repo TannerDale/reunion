f2656dee43d49e0c6347073aa0e979b67cc9bab1
Tanner Dale <tanner@tannerdale.com>
2021-06-21 13:59:43 -0500

    Add summary test

--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -57,4 +57,22 @@ RSpec.describe 'Reunion' do
 
     expect(reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
   end
+
+  it 'has a summary' do
+    reunion = Reunion.new("2107 BE")
+
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    expect(reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
+  end
 end
+g
