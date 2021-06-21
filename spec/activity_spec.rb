fb91e0c8e9884db3d0db206b9ec2977452f80ba8
Tanner Dale <tanner@tannerdale.com>
2021-06-21 13:06:46 -0500

    Add add participant and total cost tests

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe 'Activity' do
     expect(activity.name).to eq("Brunch")
     expect(activity.participants).to eq({})
   end
+
+  it 'can add a participant' do
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Maria", 20)
+
+    expect(activity.participants).to eq({"Maria" => 20})
+  end
+
+  it 'can add multiple participants' do
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.participants).to eq({"Maria" => 20, "Luther" => 40})
+  end
+
+  it 'can calculate total cost' do
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.total_cost).to eq(60)
+  end
 end
