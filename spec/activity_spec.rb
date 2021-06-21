5484e515b13002014a5fcc396ce241314699b7e0
Tanner Dale <tanner@tannerdale.com>
2021-06-21 13:00:48 -0500

    Add initialize test

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -1,1 +1,11 @@
 require './lib/activity'
+
+RSpec.describe 'Activity' do
+
+  it 'can add an activity' do
+    activity = Activity.new("Brunch")
+
+    expect(activity.name).to eq("Brunch")
+    expect(activity.participants).to eq({})
+  end
+end
