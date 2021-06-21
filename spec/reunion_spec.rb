992642d0029df3199a59038148efa1489e8203c0
Tanner Dale <tanner@tannerdale.com>
2021-06-21 14:13:07 -0500

    Add detailed breakout test

--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -74,5 +74,106 @@ RSpec.describe 'Reunion' do
 
     expect(reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
   end
+
+  it 'has a detailed breakout' do
+    reunion = Reunion.new("2107 BE")
+
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+    reunion.add_activity(activity_3)
+    reunion.add_activity(activity_4)
+
+    the_breakout = {
+      "Maria" => [
+       {
+          activity: "Brunch",
+          payees: ["Luther"],
+          amount: 10
+       },
+       {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+       },
+       {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+       },
+       {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Luther" => [
+       {
+          activity: "Brunch",
+          payees: ["Maria"],
+          amount: -10
+        },
+       {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+       {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+       {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Louis" => [
+        {
+          activity: "Drinks",
+          payees: ["Maria", "Luther"],
+          amount: 20
+       },
+       {
+          activity: "Bowling",
+          payees: ["Maria", "Luther"],
+          amount: -10
+       },
+       {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ],
+      "Nemo" => [
+       {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ]
+    }
+
+    expect(reunion.detailed_breakout).to eq(the_breakout)
+  end
 end
-g
